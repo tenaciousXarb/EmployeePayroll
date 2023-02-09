e2188200393d52ed37df046b8dc7856f28b65c36
Ahmad Arabi <ahmadarabi2562@gmail.com>
2023-02-09 12:15:47 +0600

    Default error objects added

---
+++ backapp/BLL/DTO/MainDTO/ModelStateErrorDetails.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace BLL.DTO.MainDTO
+{
+    public class ModelStateErrorDetails
+    {
+        public int Status { get; set; }
+
+        public string Title { get; set; }
+
+        public string TraceId { get; set; }
+
+        public string Type { get; set; }
+
+        public AdminDTO Errors { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
